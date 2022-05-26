a23d395581d260f675a96dbe089b642f49aa6958
Yujin Kim <yujin.kim@hotmail.fr>
2022-05-26 17:25:10 +0100

    new exon files

--- egg4_config.py
+++ egg4_config.py
@@ -79,8 +79,8 @@ mqc_config_file = "{}:file-G82027Q433Gfx69zGvjq7PqQ".format(ref_project_id)
 
 xlsx_flanks = 95
 
-cds_file = "{}:file-GB2Pk10433Gp62yK4Qk2yFXQ".format(ref_project_id)
-cds_file_for_athena = "{}:file-GB5pqz8433GXf5gP49y3xJJB".format(ref_project_id)
+cds_file = "{}:file-GB7p8x8433GVkY6F36Qx06z2".format(ref_project_id)
+cds_file_for_athena = "{}:file-GB7pXZ0433GX1YJKF5382bbX".format(ref_project_id)
 vep_config = "{}:file-GB69B1j43Vx9f0ZYGbKf9xQ1".format(ref_project_id)
 
 generate_bed_vep_stage_id = "stage-G9P8p104vyJJGy6y86FQBxkv"
